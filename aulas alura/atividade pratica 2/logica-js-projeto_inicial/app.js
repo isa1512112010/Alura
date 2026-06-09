9d43495eb28edec26cb42f87c0febbf9e0adcdbd
isa._. <isacinimartinhao@gmail.com>
2026-06-09 07:46:08 -0300

    Add files via upload

---
+++ aulas alura/atividade pratica 2/logica-js-projeto_inicial/app.js
@@ -0,0 +1,5 @@
+let Números = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+prompt("escolha um número de 1 a 10");
+if (números == 1 || números == 3 || números == 5 || números == 7 || números == 9) {
+    alert("O número é ímpar");
+} else alert("O número é par");

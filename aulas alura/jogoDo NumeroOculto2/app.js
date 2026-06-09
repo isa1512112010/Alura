e509d1171bbbc406774efff7fabb9fcd44e8a68c
isa._. <isacinimartinhao@gmail.com>
2026-06-09 09:54:18 -0300

    Add files via upload

---
+++ aulas alura/jogoDo NumeroOculto2/app.js
@@ -0,0 +1,9 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'jogo do numero secreto';
+
+let paragrafo = document.querySelector('p');
+paragrafo.innerHTML = 'Digite um numero entre 1 e 10';
+
+function verificarChute() {
+    console.log('chute verificado');
+}

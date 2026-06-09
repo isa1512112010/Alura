bca8c644d689803c72c41b013e674048e51b330e
isa._. <isacinimartinhao@gmail.com>
2026-06-09 11:28:21 -0300

    Add files via upload

--- aulas alura/jogoDo NumeroOculto2/app.js
+++ aulas alura/jogoDo NumeroOculto2/app.js
@@ -1,9 +1,51 @@
-let titulo = document.querySelector('h1');
-titulo.innerHTML = 'jogo do numero secreto';
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
 
-let paragrafo = document.querySelector('p');
-paragrafo.innerHTML = 'Digite um numero entre 1 e 10';
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto;
+}
+
+function exibirMensagemInicial() {
+exibirTextoNaTela('h1', 'jogo do numero secreto');
+exibirTextoNaTela('p', 'Digite um numero entre 1 e 10');
+}
+
+exibirMensagemInicial();
 
 function verificarChute() {
-    console.log('chute verificado');
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Parabéns, você conseguiu!');
+        let palavraTentativas = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você acertou o número secreto em ${tentativas} ${palavraTentativas}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled');
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor!');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior!');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+  return parseInt(Math.random() * 10 + 1);
+}
+
+function limparCampo() {
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo() {
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+   exibirMensagemInicial();
+   document.getElementById('reiniciar').setAttribute('disabled', true);
 }
